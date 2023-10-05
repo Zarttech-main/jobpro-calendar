7c4b19c8f23a6c56d92af8dedad5e3d482d27a38
Mubeen Abdulhameed <mohdmubeen137@gmail.com>
2023-10-05 13:46:08 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { CalendarModule } from './app/calendar.module';
 
-export module AppModule{}
+export module CalendarModule{}
 
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
